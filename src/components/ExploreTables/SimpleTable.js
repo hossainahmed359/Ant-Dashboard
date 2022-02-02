49760d800f3ff87c8c68051bd3cecf488673c583
unknown <hossainahmed359@gmail.com>
2022-02-02 13:38:42 +0600

    basic table with some filternig options

--- src/components/ExploreTables/SimpleTable.js
+++ src/components/ExploreTables/SimpleTable.js
@@ -1,7 +1,62 @@
-import { Breadcrumb } from 'antd';
-import React from 'react';
+import { Breadcrumb, Table } from 'antd';
+import React, { useEffect, useState } from 'react';
 
 const SimpleTable = () => {
+
+    // Set loaded data && Loading function && Custom pagination
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [page, setPage] = useState(1);
+    const [pageSize, setPageSize] = useState(10);
+
+    // Load data from the server
+    useEffect( () => {
+        setLoading(true)
+        fetch(`https://jsonplaceholder.typicode.com/todos`)
+            .then(res => res.json())
+            .then(data => setData(data))
+            .catch(err => console.log(err))
+            .finally(() => setLoading(false))
+    },[]);
+
+    // Set Table columns
+    const columns = [
+        {
+            title: 'Id',
+            dataIndex: 'id',
+            key: '1',
+        },
+        {
+            title: 'User Id',
+            dataIndex: 'userId',
+            key: '2',
+            // Sorter
+            sorter: (record1 , record2) => {
+                return record1.userId > record2.userId;
+            },
+        },
+        {
+            title: 'Title',
+            dataIndex: 'title',
+            key: '3',
+        },
+        {
+            title: 'Status',
+            dataIndex: 'id',
+            key: '4',
+            /* The render option gives the ability to render something dynamically or custom */
+            render: completed => {
+                return <p>{completed ? 'Complete' : 'In Progress' }</p>
+            },
+            // Filters
+            filters: [{text: 'Complete', value: true}, {text: 'In Progress', value: false}],
+            onFilter: (value, record) => {
+                return record.completed === value;
+            },
+
+        },
+    ]
+
     return (
         <div>
             <div className='start_component'>
@@ -9,7 +64,23 @@ const SimpleTable = () => {
                     <Breadcrumb.Item>Simple Table</Breadcrumb.Item>
                 </Breadcrumb>
             </div>
-            <div className='app_contents'></div>
+            <div className='app_contents'>
+                <Table
+                    loading={loading}
+                    columns={columns}
+                    dataSource={data}
+                    pagination={{
+                        current: page, 
+                        pageSize: pageSize, 
+                        total: 500,
+                        onChange:(page, pageSize) => {
+                            setPage(page);
+                            setPageSize(pageSize)
+                            // Make the api call here with page and page size
+                        } 
+                    }}
+                ></Table>
+            </div>
         </div>
     );
 };
